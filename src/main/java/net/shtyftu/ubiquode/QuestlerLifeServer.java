8fdcec41212a78041dfdd88b546678debd5d3ed0
Mikhail Koltunov <m.koltunov@pixonic.ru>
2018-02-17 14:23:51 +0300

    UTF-8 encoding

--- src/main/java/net/shtyftu/ubiquode/QuestlerLifeServer.java
+++ src/main/java/net/shtyftu/ubiquode/QuestlerLifeServer.java
@@ -3,7 +3,10 @@ package net.shtyftu.ubiquode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class QuestlerLifeServer extends SpringBootServletInitializer {
@@ -15,5 +18,15 @@ public class QuestlerLifeServer extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(QuestlerLifeServer.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("UTF-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
     }
 }
